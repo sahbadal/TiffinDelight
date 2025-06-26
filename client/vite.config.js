acda41c476a574179bc6d7a52034d59521252869
badalsah <badalsah8541@gmail.com>
2025-06-27 01:24:28 +0530

    updated base url

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/TiffinDelight",
+  base: "/TiffinDelight/",
   server: {
     port: 3001,
     open: true,
