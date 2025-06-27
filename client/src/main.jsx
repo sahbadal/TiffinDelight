af570ff8fb240d87f7904824a5e747a71d6f8c97
badalsah <badalsah8541@gmail.com>
2025-06-27 09:58:26 +0530

    updated main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/TiffinDelight">
     <AppContextProvider>
       <App />
     </AppContextProvider>
